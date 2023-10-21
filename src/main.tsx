6ec77a986285bdfb30d4d64a300b0bfd01d88801
Nathan Downer <nathandowner123@gmail.com>
2023-10-21 00:53:15 -0500

    add react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import App from './App.tsx';
 import './index.css';
 import { store } from './store/store.ts';
 import { Provider } from 'react-redux';
+import { RouterProvider } from 'react-router-dom';
+import { router } from '@/routes.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <App />
+    <RouterProvider router={router} />
   </Provider>
 );
