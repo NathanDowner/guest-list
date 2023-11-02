71a588a4da145edd9180279b1eb9a427f4784bb7
Nathan Downer <nathandowner123@gmail.com>
2023-11-01 19:21:19 -0500

    prevent users from editing other users' lists

--- src/components/GuestList.tsx
+++ src/components/GuestList.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../store/hooks';
 import { addGuest, selectList } from '../store/guestListSlice';
 import { Guest } from '../models/guest.interface';
@@ -6,32 +6,37 @@ import { createUUID } from '../utils';
 import { Droppable } from 'react-beautiful-dnd';
 import GuestCard from './GuestCard';
 import { Contributor } from '../models/contributor.interface';
+import { useAuth } from '@/contexts/AuthContext';
 
 interface GuestListProps {
   contributor: Contributor;
 }
 
-const GuestList = ({ contributor: { name } }: GuestListProps) => {
+const GuestList = ({ contributor }: GuestListProps) => {
   const [newName, setNewName] = useState('');
-
+  const { user } = useAuth();
   const dispatch = useAppDispatch();
-  const selectNamedList = selectList(name);
+  const selectNamedList = selectList(contributor.name);
   const guestList = useAppSelector(selectNamedList);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const newGuest: Guest = { name: newName, id: createUUID() };
-    dispatch(addGuest({ listName: name, guest: newGuest }));
+    dispatch(addGuest({ listName: contributor.name, guest: newGuest }));
     setNewName('');
   };
+
+  const userCanEdit = useMemo(() => {
+    return user!.email === contributor.email;
+  }, [user, contributor]);
 
   return (
     <div className="text-center w-[250px] flex-shrink-0">
       <header>
-        <h2 className="">{name}'s List</h2>
+        <h2 className="">{contributor.name}'s List</h2>
       </header>
       <div className="border border-gray-300 rounded-md p-4">
-        <Droppable droppableId={name}>
+        <Droppable droppableId={contributor.name}>
           {(provided) => (
             <div ref={provided.innerRef} {...provided.droppableProps}>
               {guestList.map((guest, index) => (
@@ -42,15 +47,17 @@ const GuestList = ({ contributor: { name } }: GuestListProps) => {
           )}
         </Droppable>
 
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            value={newName}
-            onChange={(e) => setNewName(e.target.value)}
-            placeholder="Enter a name.."
-            className="border w-full rounded-md p-1"
-          />
-        </form>
+        {userCanEdit && (
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              value={newName}
+              onChange={(e) => setNewName(e.target.value)}
+              placeholder="Enter a name.."
+              className="border w-full rounded-md p-1"
+            />
+          </form>
+        )}
       </div>
     </div>
   );
